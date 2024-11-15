1e9f4e5280d1278de1875ddfc1d17a77ed8cda70
TRB <thibault.berrido@gmail.com>
2024-11-15 16:25:08 +0000

    update: add function register_metadata_collection to improve load of custom blocks

--- inc/blocks.php
+++ inc/blocks.php
@@ -3,6 +3,11 @@
 add_action( 'init', 'df__register_blocks_types' );
 function df__register_blocks_types(): void {
 
+	wp_register_block_metadata_collection(
+		get_stylesheet_directory() . '/blocks/build/',
+		get_stylesheet_directory() . '/blocks/build/blocks-manifest.php',
+	);
+
 	foreach ( glob( get_stylesheet_directory() . '/blocks/build/*' ) as $block_directory ) {
 		register_block_type( $block_directory );
 	}
