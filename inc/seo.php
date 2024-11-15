e4ae9577b337a14b08666b8092f1bce8914cab8d
TRB <thibault.berrido@gmail.com>
2024-11-15 16:23:26 +0000

    add: head canonical + meta description

--- inc/seo.php
+++ inc/seo.php
@@ -22,15 +22,37 @@ function df__metas(): void {
 
 ?>
 
-	<meta name="description" content="<?php echo esc_html( $description ); ?>">
-	<link rel="canonical" href="<?php echo esc_html( $canonical ); ?>" />
-	<meta property="og:site_name" content="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" />
-	<meta property="og:title" content="<?php echo esc_html( $title ); ?>" />
+	<meta name="description" content="<?php echo esc_attr( $description ); ?>">
+	<link rel="canonical" href="<?php echo esc_attr( $canonical ); ?>" />
+	<meta property="og:site_name" content="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
+	<meta property="og:title" content="<?php echo esc_attr( $title ); ?>" />
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="<?php echo esc_html( $canonical ); ?>" />
-	<meta property="og:image" content="<?php echo $image; ?>" />
-	<meta property="og:description" content="<?php echo esc_html( $description ); ?>" />
+	<meta property="og:url" content="<?php echo esc_attr( $canonical ); ?>" />
+	<meta property="og:image" content="<?php echo esc_attr( $image ); ?>" />
+	<meta property="og:description" content="<?php echo esc_attr( $description ); ?>" />
 
 <?php
 
+}
+
+add_action( 'template_redirect', 'df__remove_archives' );
+
+function df__remove_archives(): void {
+	if ( is_archive() ) {
+		wp_redirect( home_url( '/' ), 301 );
+		exit;
+	}
+}
+
+add_action( 'do_feed_rdf', 'df__disable_feed', 1 );
+add_action( 'do_feed_rss', 'df__disable_feed', 1 );
+add_action( 'do_feed_rss2', 'df__disable_feed', 1 );
+add_action( 'do_feed_atom', 'df__disable_feed', 1 );
+add_action( 'do_feed_rss2_comments', 'df__disable_feed', 1 );
+add_action( 'do_feed_atom_comments', 'df__disable_feed', 1 );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'feed_links', 2 );
+
+function df__disable_feed():void {
+	wp_die( esc_attr( '<a href="' . esc_url( home_url( '/' ) ) . '">' ) );
 }
