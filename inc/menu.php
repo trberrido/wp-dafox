2f91666616065c8521ebe125956e281fb513feac
trberrido <thibault.berrido@gmail.com>
2025-01-06 18:12:57 +0000

    add: menu for footer

--- inc/menu.php
+++ inc/menu.php
@@ -1,7 +1,6 @@
 <?php defined('ABSPATH') or die();
 
 add_action( 'init', 'df__register_menu' );
-
 function df__register_menu() {
 	register_nav_menus( array( 'footer' => 'Footer navigation' ) );
 }
