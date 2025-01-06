72397dd429ed6127436d0a22d4f6c39341e5ad87
trberrido <thibault.berrido@gmail.com>
2025-01-06 18:04:02 +0000

    update: inc/blocks: separate styles related features, adding highligh for parapgraph block

--- inc/blocks.php
+++ inc/blocks.php
@@ -39,28 +39,9 @@ function df__binding_source__current_year(): string {
 }
 
 function df__binding_source__site_description(): string {
-	// cf inc/options.php
+	/*
+		Used to display site description ;
+		the data is registered here `inc/options.php`
+	*/
 	return get_option( 'df__site_description', '' );
-}
-
-add_action( 'init', 'df__enqueue_blocks_styles' );
-function df__enqueue_blocks_styles(): void {
-
-	$blocks_css_directory = '/assets/css/blocks/';
-	foreach ( glob( get_stylesheet_directory() . $blocks_css_directory . '*', GLOB_ONLYDIR ) as $directory ) {
-		$namespace = basename( $directory );
-		foreach ( glob( $directory . '/*.css' ) as $css_file ) {
-			$blockname = basename( $css_file, '.css' );
-			wp_enqueue_block_style(
-				$namespace . '/' . $blockname,
-				array(
-					'handle'	=> 'df-' . $namespace . '-' . $blockname,
-					'src'		=> get_template_directory_uri() . $blocks_css_directory . $namespace . '/' . $blockname . '.css',
-					'path'		=> get_template_directory() . $blocks_css_directory . $namespace . '/' . $blockname . '.css',
-					'ver'		=> filemtime( get_template_directory() . $blocks_css_directory . $namespace . '/' . $blockname . '.css' )
-				)
-			);
-		}
-	}
-
 }
