2d688b87e5f734476e6283327740ac2b2b9067df
TRB <thibault.berrido@gmail.com>
2025-01-07 19:55:59 +0000

    update: remove emoji feature

--- inc/seo.php
+++ inc/seo.php
@@ -64,3 +64,14 @@ add_filter(
     10,
     2
 );
+
+add_action( 'init', 'df__disable_emojis' );
+function df__disable_emojis(): void {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+}
