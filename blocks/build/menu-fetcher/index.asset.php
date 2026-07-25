81356767b5125026c031c3723e21f56ce2117168
trberrido <contact@dafox.co>
2026-07-25 23:34:42 +0000

    ci: separate block src and build

--- blocks/build/menu-fetcher/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => 'ab1874f249a0e0726fa7');
--- blocks/src/lang-switch/edit.js
+++
@@ -1,9 +0,0 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
-export default function Edit() {
-	return (
-		<p { ...useBlockProps() }>
-			Switch language : français | english
-		</p>
-	);
-}
