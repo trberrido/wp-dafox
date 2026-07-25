9dc0b7fd01d2ca8a7c9b4b558fd9083c985bc0ec
trberrido <contact@dafox.co>
2026-07-25 23:32:49 +0000

    style: remove spaces

--- functions.php
+++ functions.php
@@ -2,9 +2,7 @@
 
 add_action( 'after_setup_theme', 'df__load_includes', 5 );
 function df__load_includes(): void {
-
-	foreach ( glob( get_template_directory() . '/inc/*.php' ) as $filepath ){
+	foreach ( glob( get_template_directory() . '/functions/*.php' ) as $filepath ){
 		require_once $filepath;
 	}
-
 }
